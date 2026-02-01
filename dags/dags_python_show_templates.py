9ee6ac298fbd006b7da7a77bfb22f86a8520699c
tinahj <wjsguswl310@gmail.com>
2026-02-01 23:55:09 +0900

    python show templates 업로드

---
+++ dags/dags_python_show_templates.py
@@ -0,0 +1,22 @@
+import pendulum
+
+# Airflow 3.0 부터 아래 경로로 import 합니다.
+from airflow.sdk import DAG, task
+
+# Airflow 2.10.5 이하 버전에서 실습시 아래 경로에서 import 하세요.
+#from airflow import DAG
+#from airflow.decorators import task
+
+with DAG(
+    dag_id="dags_python_show_templates",
+    schedule="30 9 * * *",
+    start_date=pendulum.datetime(2025, 1, 20, tz="Asia/Seoul"),
+    catchup=True
+) as dag:
+    
+    @task(task_id='python_task')
+    def show_templates(**kwargs):
+        from pprint import pprint 
+        pprint(kwargs)
+
+    show_templates()
