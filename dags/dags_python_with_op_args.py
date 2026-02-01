ac3c2d289fde4b8c8f2a07055a6bc6d5f8b61190
tinahj <wjsguswl310@gmail.com>
2026-02-01 21:52:04 +0900

    op_args 테스트용 dag 업로드

---
+++ dags/dags_python_with_op_args.py
@@ -0,0 +1,25 @@
+import pendulum
+from common.common_func import regist
+
+# Airflow 3.0 부터 아래 경로로 import 합니다.
+from airflow.sdk import DAG
+from airflow.providers.standard.operators.python import PythonOperator
+
+# Airflow 2.10.5 이하 버전에서 실습시 아래 경로에서 import 하세요.
+#from airflow import DAG
+#from airflow.operators.python import PythonOperator
+
+with DAG(
+    dag_id="dags_python_with_op_args",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+    regist_t1 = PythonOperator(
+        task_id='regist_t1',
+        python_callable=regist,
+        op_args=['hjkim','man','kr','seoul']
+    )
+
+    regist_t1
