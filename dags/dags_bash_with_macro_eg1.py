0fbea4f30725e51a54528648744aa404a1fde88a
tinahj <wjsguswl310@gmail.com>
2026-02-18 23:12:58 +0900

    macro 활용

---
+++ dags/dags_bash_with_macro_eg1.py
@@ -0,0 +1,23 @@
+import pendulum
+# Airflow 3.0 부터 아래 경로로 import 합니다.
+from airflow.providers.standard.operators.bash import BashOperator
+from airflow.sdk import DAG
+
+# Airflow 2.10.5 이하 버전에서 실습시 아래 경로에서 import 하세요.
+#from airflow.operators.bash import BashOperator
+#from airflow import DAG
+
+with DAG(
+    dag_id="dags_bash_with_macro_eg1",
+    schedule="10 0 L * *", #매월 말일 수행
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    # START_DATE: 전월 말일, END_DATE: 1일 전
+    bash_task_1 = BashOperator(
+        task_id='bash_task_1',
+        env={'START_DATE':'{{ data_interval_start.in_timezone("Asia/Seoul") | ds }}',
+             'END_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=1)) | ds}}'
+        },
+        bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"'
+    )
---
+++ dags/dags_bash_with_macro_eg2.py
@@ -0,0 +1,23 @@
+import pendulum
+# Airflow 3.0 부터 아래 경로로 import 합니다.
+from airflow.providers.standard.operators.bash import BashOperator
+from airflow.sdk import DAG
+
+# Airflow 2.10.5 이하 버전에서 실습시 아래 경로에서 import 하세요.
+#from airflow.operators.bash import BashOperator
+#from airflow import DAG
+
+with DAG(
+    dag_id="dags_bash_with_macro_eg2",
+    schedule="10 0 * * 6#2", # 매월 2째주 토요일
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    # START_DATE: 2주전 월요일, END_DATE: 2주전 토요일
+    bash_task_2 = BashOperator(
+        task_id='bash_task_2',
+        env={'START_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=19)) | ds}}',
+             'END_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=14)) | ds}}'
+        },
+        bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"'
+    )
